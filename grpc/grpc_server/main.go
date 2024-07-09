24c98ddb86ca392724a20f508c2d59358ca672e6
Anton Krylov <anton0701@yandex.ru>
2024-07-09 13:51:04 +0300

    log + using getters instead of fields

--- grpc/grpc_server/main.go
+++ grpc/grpc_server/main.go
@@ -9,27 +9,38 @@ import (
 	"google.golang.org/protobuf/types/known/emptypb"
 	"log"
 	"net"
+	"strings"
 )
 
 const grpcPort = 50052
+const grpcChatApiDesc = "Chat-Api-v1"
 
 type server struct {
 	desc.UnimplementedChatV1Server
 }
 
 func (s *server) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
-	fmt.Printf("Method Create. Input params:\nUsernames: %s\n************\n\n", req.Usernames)
+	log.Println(grpcChatApiDesc)
+	log.Printf("Method Create. Input params:\nUsernames: \n%s\n************\n\n", strings.Join(req.GetUsernames(), "\n"))
+
 	resp := &desc.CreateResponse{
 		Id: 1,
 	}
+
 	return resp, nil
 }
+
 func (s *server) Delete(ctx context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
-	fmt.Printf("Method Delete. Input params:\nId: %d\n************\n\n", req.Id)
+	log.Println(grpcChatApiDesc)
+	log.Printf("Method Delete. Input params:\nId: %d\n************\n\n", req.GetId())
+
 	return &emptypb.Empty{}, nil
 }
+
 func (s *server) SendMessage(ctx context.Context, req *desc.SendMessageRequest) (*emptypb.Empty, error) {
-	fmt.Printf("Method SendMessage. Input params:\nFrom: %s\nText: %s\nTimestamp: %s************\n\n", req.GetFrom(), req.GetText(), req.GetTimestamp())
+	log.Println(grpcChatApiDesc)
+	log.Printf("Method SendMessage. Input params:\nFrom: %s\nText: %s\nTimestamp: %s************\n\n", req.GetFrom(), req.GetText(), req.GetTimestamp())
+	
 	return &emptypb.Empty{}, nil
 }
 
