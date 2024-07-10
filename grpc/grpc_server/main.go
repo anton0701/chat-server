c97b61f63e8806bc9b19c35113dd4b3172850297
Anton Krylov <anton0701@yandex.ru>
2024-07-10 16:32:12 +0300

    fix after PR review

--- grpc/grpc_server/main.go
+++ grpc/grpc_server/main.go
@@ -3,45 +3,23 @@ package main
 import (
 	"context"
 	"fmt"
-	desc "github.com/anton0701/chat-server/grpc/pkg/chat_v1"
+	"log"
+	"net"
+
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"google.golang.org/protobuf/types/known/emptypb"
-	"log"
-	"net"
-	"strings"
+
+	desc "github.com/anton0701/chat-server/grpc/pkg/chat_v1"
 )
 
-const grpcPort = 50052
-const grpcChatApiDesc = "Chat-Api-v1"
+const (
+	grpcPort        = 50052
+	grpcChatApiDesc = "Chat-Api-v1"
+)
 
 type server struct {
 	desc.UnimplementedChatV1Server
-}
-
-func (s *server) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
-	log.Println(grpcChatApiDesc)
-	log.Printf("Method Create. Input params:\nUsernames: \n%s\n************\n\n", strings.Join(req.GetUsernames(), "\n"))
-
-	resp := &desc.CreateResponse{
-		Id: 1,
-	}
-
-	return resp, nil
-}
-
-func (s *server) Delete(ctx context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
-	log.Println(grpcChatApiDesc)
-	log.Printf("Method Delete. Input params:\nId: %d\n************\n\n", req.GetId())
-
-	return &emptypb.Empty{}, nil
-}
-
-func (s *server) SendMessage(ctx context.Context, req *desc.SendMessageRequest) (*emptypb.Empty, error) {
-	log.Println(grpcChatApiDesc)
-	log.Printf("Method SendMessage. Input params:\nFrom: %s\nText: %s\nTimestamp: %s************\n\n", req.GetFrom(), req.GetText(), req.GetTimestamp())
-	
-	return &emptypb.Empty{}, nil
 }
 
 func main() {
@@ -59,4 +37,24 @@ func main() {
 	if err = s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
+}
+
+func (s *server) Create(_ context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
+	log.Printf("%s\nMethod Create.\nInput params:\n%+v\n************\n\n", grpcChatApiDesc, req)
+
+	return &desc.CreateResponse{
+		Id: 1,
+	}, nil
+}
+
+func (s *server) Delete(_ context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
+	log.Printf("%s\nMethod Delete.\nInput params:\n%+v\n************\n\n", grpcChatApiDesc, req)
+
+	return &emptypb.Empty{}, nil
+}
+
+func (s *server) SendMessage(_ context.Context, req *desc.SendMessageRequest) (*emptypb.Empty, error) {
+	log.Printf("%s\nMethod SendMessage.\nInput params:\n%+v\n************\n\n", grpcChatApiDesc, req)
+
+	return &emptypb.Empty{}, nil
 }
