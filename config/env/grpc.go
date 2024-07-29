6ee26ee85b98b39df72567c587e3b15c0b483ede
Anton Krylov <anton0701@yandex.ru>
2024-07-29 19:17:58 +0300

    добавлены описания публичных методов, интерфейсов и структур

--- config/env/grpc.go
+++ config/env/grpc.go
@@ -12,15 +12,27 @@ const (
 	grpcPortEnvName = "GRPC_PORT"
 )
 
+// GRPCConfig - интерфейс конфига для инициализации GRPC-сервера.
+//
+// Методы:
+//   - Address() string: адрес, на котором развернут GRPC-сервер в формате "хост:порт".
 type GRPCConfig interface {
 	Address() string
 }
 
+// grpcConfig - структура конфига GRPC-сервера, реализующая интерфейс GRPCConfig.
 type grpcConfig struct {
 	host string
 	port string
 }
 
+// NewGRPCConfig - Метод для создания объекта конфига GRPC-сервера, реализующего
+// интерфейс GRPCConfig.
+// Параметры конфига берутся из переменных окружения программы.
+//
+// Возвращает:
+//   - GRPCConfig: созданный объект конфига GRPC-сервера.
+//   - error: ошибка, если что-то пошло не так.
 func NewGRPCConfig() (GRPCConfig, error) {
 	host := os.Getenv(grpcHostEnvName)
 	if len(host) == 0 {
